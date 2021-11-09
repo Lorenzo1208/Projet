0b201d0c868606dce7e97649d7e70a6bbc300d72
Lorenzo <Lorenzo.gurgo@gmail.com>
2021-11-09 15:29:01 +0100

    a

---
+++ src/Classe/Search.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Classe;
+
+use App\Entity\Category;
+
+class Search 
+
+{
+
+    /**
+     * @var string
+     */
+
+    public $string = '';
+
+    /**
+     * @var Category[]
+     */
+
+    public $categories = [];
+}
