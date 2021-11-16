221b3fcf64b043b174708a401a3011f325bbaf8b
thomas angama <thomasangama77460@gmail.com>
2021-11-16 19:00:42 +0100

    redirection apres connection thomas

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -51,8 +51,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-    return new RedirectResponse($this->urlGenerator->generate('account'));
-      
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl(Request $request): string
