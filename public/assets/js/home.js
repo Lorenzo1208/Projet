e09fe6f38fffe0ce427bb63dba75d78ed860188e
Lorenzo <lorenzo.gurgo.com>
2021-11-23 15:32:30 +0100

    animation home page + a href logo

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,3 +1,11 @@
-$('html, body').animate({
-	scrollTop: $("#page-2").offset().top
-}, 15000);
+
+
+$('.btn--primary').click(function(event) {
+    // Preventing default action of the event
+    event.preventDefault();
+    // Getting the height of the document
+    var n = $(document).height();
+	$('html, body').animate({
+		scrollTop: $("#page-2").offset().top
+	}, 2000);                                 
+});
