513b8633f475610e44ac09bb36f2ca975d503e91
Lorenzo <lorenzo.gurgo.com>
2021-11-23 11:41:28 +0100

    scroll

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,11 +1,5 @@
-
-
-	$(document).ready(function() {
-		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
-			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-			return false;
-		});
-	});
-
+$("#scroll-down").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".timeline-wrapper").offset().top
+    }, 1500);
+  });
