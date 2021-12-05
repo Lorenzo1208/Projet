d08ef65e82ce523e54e9f322a0565b393d1d3139
Lorenzo <lorenzo.gurgo.com>
2021-12-05 18:15:19 +0100

    contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
+use App\Entity\ResetPassword;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,8 +28,40 @@ class ContactController extends AbstractController
             $this->addFlash('notice', 'Merci de nous avoir contacté. Notre équipe va vous répondre dans les meilleurs délais.');
         }
 
+        if ($request->get('email')) {
+            $user = $this->entityManager->getRepository(User::class)->findOneByEmail($request->get('email'));
+
+            if ($user) {
+                // 1 : Enregistrer en base la demande de reset_password avec user, token, createdAt.
+                $reset_password = new ResetPassword();
+                $reset_password->setUser($user);
+                $reset_password->setToken(uniqid());
+                $reset_password->setCreatedAt(new \DateTime());
+                $this->entityManager->persist($reset_password);
+                $this->entityManager->flush();
+
+                // 2 : Envoyer un email à l'utilisateur avec un lien lui permettant de mettre à jour son mot de passe.
+                $url = $this->generateUrl('update_password', [
+                    'token' => $reset_password->getToken()
+                ]);
+
+                $content = "Bonjour ".$user->getFirstname()."<br/>Vous avez demandé à réinitialiser votre mot de passe sur le site d'Antonin Fournet.<br/><br/>";
+                $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='".$url."'>mettre à jour votre mot de passe</a>.";
+
+                $mail = new Mail();
+                $mail->send($user->getEmail(), $user->getFirstname().' '.$user->getLastname(), 'Réinitialiser votre mot de passe sur le site', $content);
+
+                $this->addFlash('notice', 'Vous allez recevoir dans quelques secondes un mail avec la procédure pour réinitialiser votre mot de passe.');
+            } else {
+                $this->addFlash('notice', 'Cette adresse email est inconnue.');
+            }
+        }
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+
+      
+
 }
