98c88d0e35ae3ded710c0f7e3670e2f4e499d547
Yvann <yvann.kreutzenberger@hotmail.fr>
2021-12-07 10:43:00 +0100

    finalisation des fonctionnalités

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -38,26 +38,7 @@ class RegisterController extends AbstractController
 
             $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
 
-            if (!$search_email) {
-                $password = $encoder->encodePassword($user,$user->getPassword());
-
-                $user->setPassword($password);
-
-                $this->entityManager->persist($user);
-                $this->entityManager->flush();
-
-                $mail = new Mail();
-                $content = "Bonjour ".$user->getFirstname()."<br/>Bienvenue sur le site d'Antonin Fournet.<br><br/>Si vous avez des questions contacter directement sur le site !";
-                $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur le site ! ', $content);
-
-                $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
-            } else {
-                $notification = "L'email que vous avez renseigné existe déjà.";
-            }
-
-            
-                $this->addFlash('notice', "Merci d'avoir créer un compte, vous pouvez maitenant vous connecter !");
-                return $this->redirectToRoute('app_login');
+                  return $this->redirectToRoute('app_login');
         }
 
         return $this->render('register/index.html.twig', [
